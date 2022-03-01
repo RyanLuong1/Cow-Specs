ab49808ee4de5890f8d60d05ae0ab22ee643ee6e
maikhol <michaeltrinh@csu.fullerton.edu>
2022-02-28 20:20:19 -0800

    file contains cpu information

---
+++ system_info/cpu_information.py
@@ -0,0 +1,29 @@
+import platform
+import psutil
+
+# CPU physical cores
+def core_count0():
+    return {psutil.cpu_count(logical = False)}
+
+# CPU Logical cores (for Hyperthreading)
+def core_count1():
+    return {psutil.cpu_count(logical = True)}
+
+# Current CPU Frequency
+def cpu_freq():
+    return {psutil.cpu_freq().current}
+
+# Minimum CPU Frequency
+def cpu_freq_min():
+    return {psutil.cpu_freq().min}
+
+# Maximum CPU Frequency
+def cpu_fre_max():
+    return {psutil.cpu_freq().max}
+
+# Current CPU Untilization (Usage)
+def cpu_usage():
+    return {psutil.cpu_percent(interval = 1)}
+
+# CPU Tempature (measured in celsius)
+#cpu_temp =
