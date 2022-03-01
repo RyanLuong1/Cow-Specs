05b4976dc5eaf1ab0e749f63208920692aeed738
maikhol <michaeltrinh@csu.fullerton.edu>
2022-02-28 20:19:18 -0800

    file contains motherboard, memory, storage information

--- system_info/system_information.py
+++ system_info/system_information.py
@@ -57,16 +57,19 @@ def cpu_usage():
 
 # Total Ram installed on the system
 # rounds to the nearest decimal place (needs to be double checked)
-{round(psutil.virtual_memory().total/1000000000000, 2)}
+def total_ram():
+    return {round(psutil.virtual_memory().total/1000000000000, 2)}
 
 # Avaliable Ram
-{round(psutil.virtual_memory().avaliable/1000000000000, 2)}
+def avaliable_ram():
+    return {round(psutil.virtual_memory().avaliable/1000000000000, 2)}
 
 # Used Ram
 # {round(psutil.virtual_memory().used/1000000000000, 2)}
 
 # Ram Untilization
-{psutil.virtual_memory().percent}
+def memory_usage():
+    return {psutil.virtual_memory().percent}
 
 
 # GPU Information
