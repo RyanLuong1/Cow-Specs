50d899ca1a75a626ecbaf556bb71562a3ae044f1
Ryan Luong <ryanluong83@gmail.com>
2022-03-09 12:43:25 -0800

    Hardcoded the rows
    Hardcoded the rows. Starting to program the children as well

--- system_info/widget.py
+++ system_info/widget.py
@@ -30,9 +30,18 @@ class Widget(QWidget):
     #        self.tree_view.topLevelItem(3).child(2).setText(3, str(gpu_temp().pop()))
 
     def text_display(self):
-        self.tree_view.topLevelItem(1).child(0).setText(1, str(cpu_freq().pop()))
-        self.tree_view.topLevelItem(1).child(0).setText(2, str(cpu_freq_min().pop()))
-        self.tree_view.topLevelItem(1).child(0).setText(3, str(cpu_fre_max().pop()))
+        hardwares = ["Motherboard", "CPU", "RAM", "GPU"]
+        motherboard_items = ["Voltages", "Temperatures", "Fans", "Controls"]
+        CPU_items = ["Clocks", "Temperatures", "Powers", "Memory"]
+        RAM_items = ["Load", "Data"]
+        GPU_items = ["Clocks", "Temperatures", "Load", "Fans", "Controls", "Powers", "Data"]
+        hardwares = ["Motherboard", "CPU", "GPU", "Storage", "Network", "RAM"]
+        for hardware in hardwares[::-1]:
+            parent = QtWidgets.QTreeWidgetItem([hardware])
+            self.tree_view.insertTopLevelItem(0, parent)
+#        self.tree_view.topLevelItem(1).child(0).setText(1, str(cpu_freq().pop()))
+#        self.tree_view.topLevelItem(1).child(0).setText(2, str(cpu_freq_min().pop()))
+#        self.tree_view.topLevelItem(1).child(0).setText(3, str(cpu_fre_max().pop()))
 
     def core_program(self):
         self.text_display()
