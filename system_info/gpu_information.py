02f176da18fb6490df109f2308688adc37dc6fbe
maikhol <michaeltrinh@csu.fullerton.edu>
2022-03-17 14:25:36 -0700

    Fixed calling GPUtil functions

--- system_info/gpu_information.py
+++ system_info/gpu_information.py
@@ -1,32 +1,18 @@
 import platform
 import GPUtil
 
+
 # GPU Information
-# GPU ID: Displays which gpu is installed on PCIE slot
-def gpu_id():
-    return GPUtil.id
+GPU = GPUtil.getGPUs()[0]
 
 # GPU Name
 def gpu_name():
-    return GPUtil.name
+    return GPU.name 
 
 # GPU Utilization
 def gpu_usage():
-    return GPUtil.showUtilization()
-
-# GPU Free Memory
-# This wont show each gpu usage
-def gpu_free_mem():
-    return GPUtil.memoryFree
-
-# GPU Used Memory
-def gpu_used_mem():
-    return GPUtil.memoryUsed
-
-# GPU Total Memory
-def gpu_mem():
-    return GPUtil.memoryTotal
+    return GPU.load*100
 
 # GPU Tempature (measured in celsius)
 def gpu_temp():
-    return GPUtil.temperature
+    return GPU.temperature
