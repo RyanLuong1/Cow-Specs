9183b22681a8507b7dbabcb2d83934d3615fdd30
/home/luigi/.gitconfig <lgibertoni@csu.fullerton.edu>
2022-03-16 22:56:46 -0700

    fixed up cpu_freq, returns 2d array with 1st array being cores, and 2nd array being temps (current,min,max)

--- system_info/cpu_information.py
+++ system_info/cpu_information.py
@@ -18,16 +18,19 @@ def core_count0():
 def core_count1():
     return psutil.cpu_count(logical = True)
 
-# Current CPU Frequency
+# Current cpu freq
 def cpu_freq():
-    return psutil.cpu_freq(percpu=True)
-
-def extract_info_from_freq():
-    #we need to take out the information from cpu_freq(), it's all garbled up
-    cpu_freq_decoded = cpu_freq()
+    '''Take out information from psutil.cpu_freq and return it as a 2d list, 
+    the info in cpu_freq is garbled up and this essentially seperates it and cleans it up for easier access'''
+    cpu_freq_decoded = psutil.cpu_freq(percpu=True)
+    core = []
+    coretemp = []
     for index in range(len(cpu_freq_decoded)):
         for j in range(0,3): #0 is current, 1 is min, 2 is max
-            print(cpu_freq_decoded[index][j]) #cpu_freq_decoded[index][j], index is core, j is temp type
+            coretemp.append(cpu_freq_decoded[index][j]) #cpu_freq_decoded[index][j], index is core, j is temp type
+        core.append(coretemp)
+        coretemp = []
+    return core #returns 2d array of [core] and [temp]
 
 # Current CPU Untilization (Usage)
 def cpu_usage():
